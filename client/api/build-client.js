fcb4c0641788e0f89d4a452def5b762db7aa4961
Betty <s9654025@gmail.com>
2024-03-20 09:23:26 +0800

    update config

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ const buildClient = ({ req }) => {
 
     return axios.create({
       baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        "http://www.findhotel.site",
       headers: req.headers,
     });
   } else {
